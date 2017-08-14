8cf9211ebdb59cab2da632a13427628511ff4032
Iqbal <iqbalchoudhury2@gmail.com>
2017-08-14 18:59:29 -0500

    scroll added

--- src/test/java/com/appium/android/appium_android_framework/TestBase.java
+++ src/test/java/com/appium/android/appium_android_framework/TestBase.java
@@ -7,12 +7,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -40,10 +44,61 @@ public class TestBase {
 		ad.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		
 	}
-	
+	/**
+	 * Drag and drop
+	 * 
+	 */
 	@Test
 	public void Test001() {
-		System.out.println("Running empyt test");
+		ad.findElement(By.xpath("//android.widget.TextView[@text='Views']")).click();
+	//Drag and Drop
+		ad.findElement(By.xpath("//android.widget.TextView[@text='Drag and Drop']")).click();
+		TouchAction action=new TouchAction(ad);
+		
+		action.longPress(ad.findElement(By.id("io.appium.android.apis:id/drag_dot_1"))).moveTo(ad.findElement(By.id("io.appium.android.apis:id/drag_dot_3"))).release().perform();
+		
+	//	ad.findElementById("io.appium.android.apis:id/drag_dot_1")
+	
+		//System.out.println("Running empyt test");
+		
+	}
+	
+	@Test
+	public void Test002() {
+		Dimension size=ad.manage().window().getSize();
+		
+		//drag and drop
+		ad.findElement(By.xpath("//android.widget.TextView[@text='Views']")).click();
+		//		ad.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));");
+		//ad.findElementByAndroidUIAutomator("new UiScrollable (new UiSelector()).scrollIntoView(text(\"Tabs\"));");
+		ad.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Tabs\"));");
+		ad.findElement(By.xpath("//android.widget.TextView[@text='Tabs']")).click();
+
+		ad.findElement(By.xpath("//android.widget.TextView[@text='5. Scrollable']")).click();
+		int startx=(int) (size.width*0.70);
+		int endx=(int) (size.width*0.30);
+		Point point = ad.findElement(By.xpath("//android.widget.TextView[@text='Tab 1']")).getLocation();
+	int xlookingfor;
+	System.out.println( xlookingfor=size.getHeight());
+	int firstone;
+	System.out.println( firstone=point.getX());
+		int starty=point.getY()+2;
+		
+		ad.swipe(startx, starty, endx,starty, 2000);
+		//boolean run=true;
+		
+		
+		for(int i=0;i<10;i++) {
+			if((ad.findElements(By.xpath("//android.widget.TextView[@text='Tab 18']")).size()==0)) {
+				
+				
+				 ad.swipe(startx, starty, endx,starty, 2000);
+				}
+			else {
+				ad.findElement(By.xpath("//android.widget.TextView[@text='Tab 18']")).click();
+			}
+		}
+		 
 		
 	}
 	@AfterMethod
